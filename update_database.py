4ae8f4485a8ea89683bc28d10bf4323dfe906ee5
Andrew K <fybe@fybe.dev>
2026-02-24 14:51:40 +0900

    update workflow to test before pushing to containers

--- update_database.py
+++ update_database.py
@@ -1,3 +1,8 @@
 from tools.db_auth import register_user
+from lightdb import LightDB
+
+db = LightDB()
+db['users'] = []
+db['files'] = {}
 
 register_user('admin', 'admin', quota_gb=10, is_admin=True)
